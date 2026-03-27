b68effe92e51c20f1d7bb28466d3574c7ff9e904
j-Riv <jose.alfredo.rivera@gmail.com>
2026-03-27 15:09:02 -0700

    chore: Removed placeholder="blur" possible temp fix for netlify image loading issue

--- components/Banner/Banner.js
+++ components/Banner/Banner.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import Image from 'next/image';
-import bannerImage from '../../images/footer-banner.png';
+import React from 'react'
+import Image from 'next/image'
+import bannerImage from '../../images/footer-banner.png'
 
 export default function Banner() {
   return (
     <section>
-      <Image src={bannerImage} alt="Vicente Sarmiento for OC Supervisor. Election Day June 7" placeholder="blur" layout="responsive" />
+      {/* Removed placeholder="blur" possible temp fix for netlify image loading issue */}
+      <Image
+        src={bannerImage}
+        alt="Vicente Sarmiento for OC Supervisor. Election Day June 7"
+        layout="responsive"
+      />
     </section>
   )
 }
