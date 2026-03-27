f09b4022766a18116872e591c4acb651962fb78c
j-Riv <jose.alfredo.rivera@gmail.com>
2026-03-27 16:14:43 -0700

    refactor: Temorarily using html images in favor of next/image compnoents until we upgrade to a current version of nextjs

--- pages/district-map.js
+++ pages/district-map.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
-import Image from 'next/image'
+// import Image from 'next/image'
 import Layout from '../components/layout'
 import map from '../images/district-map.png'
 
@@ -9,18 +9,28 @@ export default function DistrictMap() {
     <Layout>
       <Head>
         <title>District Map | Sarmiento for OC Supervisor</title>
-        <meta name="description" content="Vicente Sarmiento is running for Orange County Supervisor" />
+        <meta
+          name="description"
+          content="Vicente Sarmiento is running for Orange County Supervisor"
+        />
         <meta
           property="og:image"
           content="/sarmiento-headshot-watermarked.jpg"
         />
-        <meta name="og:title" content="Sarmiento For Orange County Supervisor" />
+        <meta
+          name="og:title"
+          content="Sarmiento For Orange County Supervisor"
+        />
         <meta name="twitter:card" content="summary_large_image" />
         <link rel="icon" href="/favicon.png" />
       </Head>
       <div className="mt-10 pt-6 sm:mt-4 max-w-4xl mx-auto">
         <h1>Orange County District 2 Map</h1>
-        <Image src={map} alt="Second District, County Of Orange, California" />
+        {/* <Image src={map} alt="Second District, County Of Orange, California" /> */}
+        <img
+          src={map.src}
+          alt="Second District, County Of Orange, California"
+        />
       </div>
     </Layout>
   )
