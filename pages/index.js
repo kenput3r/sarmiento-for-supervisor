5e8f38fb1d1aab6e7b41b8ed8930378c9c178dab
j-Riv <jose.alfredo.rivera@gmail.com>
2026-03-27 12:25:31 -0700

    chore: Commented out PressRElease, Updates, Video and Videos

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,46 @@
 import React from 'react'
 import Head from 'next/head'
 import Layout from '../components/layout'
-import { useAppContext } from '../components/context'
+// import { useAppContext } from '../components/context'
 import Hero from '../components/Hero/Hero'
-import PressRelease from '../components/PressRelease'
+// import PressRelease from '../components/PressRelease'
 import Priorities from '../components/Priorities/Priorities'
-import Updates from '../components/Updates/Updates'
-import Video from '../components/Video/Video'
-import Videos from '../components/Videos/Videos'
+// import Updates from '../components/Updates/Updates'
+// import Video from '../components/Video/Video'
+// import Videos from '../components/Videos/Videos'
 
-const videoUrl = {
-  english: 'https://www.youtube.com/embed/FvFVZrcQr6Y',
-  spanish: 'https://www.youtube.com/embed/FGYSubz_k_k',
-}
+// const videoUrl = {
+//   english: 'https://www.youtube.com/embed/FvFVZrcQr6Y',
+//   spanish: 'https://www.youtube.com/embed/FGYSubz_k_k',
+// }
 
-const videos = ['https://www.youtube.com/embed/1LdapAhTg0c', 'https://www.youtube.com/embed/6zfDLJzAVGI', 'https://www.youtube.com/embed/Ijjt8jX-J4Q', 'https://www.youtube.com/embed/_2b6mf-6IkA']
+// const videos = ['https://www.youtube.com/embed/1LdapAhTg0c', 'https://www.youtube.com/embed/6zfDLJzAVGI', 'https://www.youtube.com/embed/Ijjt8jX-J4Q', 'https://www.youtube.com/embed/_2b6mf-6IkA']
 
 export default function Home() {
-  const { language } = useAppContext()
+  // const { language } = useAppContext()
 
   return (
-
     <Layout home>
       <Head>
         <title>Sarmiento For OC Supervisor</title>
-        <meta name="description" content="Vicente Sarmiento is running for Orange County Supervisor" />
+        <meta
+          name="description"
+          content="Vicente Sarmiento is running for Orange County Supervisor"
+        />
         <link rel="icon" href="/favicon.png" />
       </Head>
 
       <Hero />
 
-      <PressRelease />
+      {/* <PressRelease /> */}
 
       <Priorities />
 
-      <Updates />
+      {/* <Updates /> */}
 
-      <Video url={videoUrl[language]} classes="pt-6 phones:py-0 phones:mt-4 phones:mx-4" />
+      {/* <Video url={videoUrl[language]} classes="pt-6 phones:py-0 phones:mt-4 phones:mx-4" /> */}
 
-      <Videos videos={videos} />
-
+      {/* <Videos videos={videos} /> */}
     </Layout>
-
   )
 }
